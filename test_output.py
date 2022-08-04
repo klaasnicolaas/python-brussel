f0004d394de1b0d65a6f341ed09ee5c79b6607bd
Klaas Schoute <klaas_schoute@hotmail.com>
2022-08-04 01:29:51 +0000

    Add unique ID counter

--- test_output.py
+++ test_output.py
@@ -11,14 +11,22 @@ async def main() -> None:
     async with ODPBrussel() as client:
         garages = await client.garages(limit=25)
         disabled_parkings = await client.disabled_parkings(limit=1000)
-
         print(garages)
 
         count: int
         for index, item in enumerate(disabled_parkings, 1):
             count = index
             print(item)
-        print(f"{count} locations found")
+
+        # Count unique id's in disabled_parkings
+        unique_values: list[str] = []
+        for item in disabled_parkings:
+            unique_values.append(item.spot_id)
+        num_values = len(set(unique_values))
+
+        print("__________________________")
+        print(f"Total locations found: {count}")
+        print(f"Unique ID values: {num_values}")
 
 
 if __name__ == "__main__":
